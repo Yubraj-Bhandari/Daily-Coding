b10656684f1536e3f4f5bf6566073c24ddd1cb45
Yubraj bhandari <uvrajbhandari1@gmail.com>
2024-09-08 21:58:12 +0545

    Added index.js file

---
+++ Day2/index.js
@@ -0,0 +1,21 @@
+console.log("Hello world");
+console.log(process.platform)
+
+
+
+// Just learned the basic concept and installation of nodejs
+
+// Nodejs is a Non-Blocking I/O as it can run multiple operation
+// at same Time
+// is a cross platform runtime 
+// package manager used to manage install download and assist
+//  computer  programs
+
+//  Js= npm,yarn,pnpm 
+  
+//  REPL= Read Eval(evaluate) Print Loop= environment or console window 
+
+//  built in globals= 
+//  global , process , console,settimeout(), 
+
+//  pseudo globals = used for files specific 
